a4aba95aae597843c58e57b4737a25b0e74914d0
MRmikhaell <139768149+MRmikhaell@users.noreply.github.com>
2023-08-06 00:19:22 +0700

    Update tambah_data_karyawan.php

--- tambah_data_karyawan.php
+++ tambah_data_karyawan.php
@@ -4,12 +4,16 @@ error_reporting(0);
 // $roleKaryawan = $_POST['role'];
 // echo $roleKaryawan ;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $namaKaryawan = $_POST['nama_karyawan'];
+    $namaKaryawan = strtolower($_POST['nama_karyawan']);
     $notelpKaryawan = $_POST['no_telp'];
-    $alamatKaryawan = $_POST['alamat'];
-    $usernameKaryawan = $_POST['username'];
+    $alamatKaryawan = strtolower($_POST['alamat']);
+    $usernameKaryawan = strtolower($_POST['username']);
     $passwordKaryawan = $_POST['password'];
     $roleKaryawan = $_POST['role'];
+
+
     insertKaryawan($namaKaryawan, $notelpKaryawan, $alamatKaryawan, $usernameKaryawan, $passwordKaryawan, $roleKaryawan);
+
+
   }
 ?>
