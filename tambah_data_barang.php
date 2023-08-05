8804bc4d09a93595d6a9730b1a89ba92db332fb4
MRmikhaell <139768149+MRmikhaell@users.noreply.github.com>
2023-08-06 00:18:25 +0700

    Update tambah_data_barang.php

--- tambah_data_barang.php
+++ tambah_data_barang.php
@@ -3,10 +3,10 @@ require 'header.php';
 // Sembunyikan semua pesan error
 error_reporting(0);
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $namaBarang = $_POST['nama_barang'];
+  $namaBarang = strtolower($_POST['nama_barang']);
   $jumlahBarang = $_POST['jumlah_barang'];
   $hargaBarang = $_POST['harga_barang'];
-  $satuanBarang = $_POST['satuan_barang'];
+  $satuanBarang = strtolower($_POST['satuan_barang']);
   insertBarang($namaBarang, $hargaBarang, $jumlahBarang, $satuanBarang);
 }
 ?>
