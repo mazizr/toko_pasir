26b92e6a013aefad7c105ddd5acdd4425bdbc85b
MRmikhaell <139768149+MRmikhaell@users.noreply.github.com>
2023-08-06 00:13:48 +0700

    Update edit_data_barang.php

--- edit_data_barang.php
+++ edit_data_barang.php
@@ -4,10 +4,10 @@
   // error_reporting(0);
   if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $idBarang = $_POST['id_barang'];
-    $namaBarang = $_POST['nama_barang'];
+    $namaBarang = strtolower($_POST['nama_barang']);
     $jumlahBarang = $_POST['jumlah_barang'];
     $hargaBarang = $_POST['harga_barang'];
-    $satuanBarang = $_POST['satuan_barang'];
+    $satuanBarang = strtolower($_POST['satuan_barang']);
     editBarang($idBarang,$namaBarang, $hargaBarang, $jumlahBarang, $satuanBarang);
   }
 ?>
